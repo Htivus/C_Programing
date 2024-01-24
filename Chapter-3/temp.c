9dbab27bb2b649a425b2c23aa9a111b8cb0c3bae
Htivus <suvithkumar0000@gmail.com>
2024-01-24 22:14:03 +0530

    chapter 3 start

---
+++ Chapter-3/temp.c
@@ -0,0 +1,44 @@
+/* WELCOME TO C PROGRAMMING SUVITH */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int c, i, nwhite, nother, ndigit[10];
+
+	nwhite = nother = 0;
+
+	for (i = 0; i < 10; i++)
+	ndigit[i] = 0;
+
+	while ( (c = getchar()) != EOF )
+	{
+		switch (c)
+		{
+		case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':case '0':
+			ndigit[ c - '0' ]++;
+			break;
+
+		case ' ':
+		case '\t':
+		case '\n':
+			nwhite++;
+			break;
+
+		default :
+			nother++;
+			break;
+		}
+	}
+	
+	printf("\n\ndigits =");
+	for (i = 0; i < 10; i++)
+		printf(" %d",ndigit[i]);
+	printf("\n White space %d , Other %d \n\n", nwhite, nother); 	
+
+	return 0;
+}
+
+
--- Rough/lib.c
+++
@@ -1,8 +0,0 @@
-/* WELCOME TO C PROGRAMMING SUVITH */
-// mylib.c
-#include "mylib.h"
-
-int addTwoNumbers(int a, int b) {
-    return a + b;
-}
-
