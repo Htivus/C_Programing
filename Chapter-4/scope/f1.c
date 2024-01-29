06b084f80b25f78f399a32633653ab93ef4e12dc
Htivus <suvithkumar0000@gmail.com>
2024-01-29 06:23:24 +0530

    27/1

---
+++ Chapter-4/scope/f1.c
@@ -0,0 +1,17 @@
+/* WELCOME TO C PROGRAMMING SUVITH */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "f2.h"
+
+
+int b= 20;
+
+
+int main()
+{
+	printf ("f1: a = %d \t b = %d\n",a,b);
+	return 0;
+}
+
+
---
+++ Chapter-4/scope/f2.h
@@ -0,0 +1,2 @@
+static int a=100;
+extern int b;
